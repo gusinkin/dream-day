940b52925d43290d59f8e577800e676ec2d31809
Dmitry Gusinkin <d.gusinkin@gmail.com>
2023-09-29 21:54:04 +0300

    hero phone size

--- src/components/HeroSection.tsx
+++ src/components/HeroSection.tsx
@@ -12,17 +12,13 @@ export const HeroSection = () => {
     <section className={`${styles.section} ${styles.heroSection}`}>
       {/* <div className={styles.section__container}> */}
       <div className={styles.heroSection__content}>
-        <h3 className={styles.heroSection__subtitle}>
-          СОЗДАНИЕ СЧАСТЛИВЫХ МОМЕНТОВ
-        </h3>
+        <h3 className={styles.heroSection__subtitle}>СОЗДАНИЕ СЧАСТЛИВЫХ МОМЕНТОВ</h3>
         <h1 className={styles.heroSection__title}>DREAM DAY</h1>
-        <h3 className={styles.heroSection__subtitle}>
-          ВОЗДУШНЫЕ ШАРЫ И ОФОРМЛЕНИЕ
-        </h3>
+        <h3 className={styles.heroSection__subtitle}>ВОЗДУШНЫЕ ШАРЫ И ОФОРМЛЕНИЕ</h3>
         <p className={styles.heroSection__text}>
-          МЫ – КОМАНДА DREAM DAY. УЖЕ НЕСКОЛЬКО ЛЕТ МЫ СОЗДАЕТ КРАСОЧНЫЕ
-          КОМПОЗИЦИИ ИЗ ВОЗДУШНЫХ ШАРОВ, ФОТОЗОНЫ, ОФОРМЛЯЕМ ПРАЗДНИКИ И
-          МЕРОПРИЯТИЯ. НАША ЗАДАЧА – СДЕЛАТЬ ВАШ ПРАЗДНИК ЯРЧЕ!
+          МЫ – КОМАНДА DREAM DAY. УЖЕ НЕСКОЛЬКО ЛЕТ МЫ СОЗДАЕМ КРАСОЧНЫЕ КОМПОЗИЦИИ ИЗ ВОЗДУШНЫХ
+          ШАРОВ, ФОТОЗОНЫ, ОФОРМЛЯЕМ ПРАЗДНИКИ И МЕРОПРИЯТИЯ. НАША ЗАДАЧА – СДЕЛАТЬ ВАШ ПРАЗДНИК
+          ЯРЧЕ!
         </p>
         <button className={styles.button} onClick={scrollDown}>
           ПОДОБРАТЬ ШАРИКИ
